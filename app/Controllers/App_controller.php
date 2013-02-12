5be122db3df71ea205398a58bd8ee616a4378512
fpumir <fpumir@terredeweb.fr>
2013-02-12 22:30:01 +0100

    initial commit
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -1,60 +1,18 @@
 <?php
 class App_controller{
-  
-  function home(){
-    $id=F3::get('PARAMS.id');
-    $location=App::instance()->getLocationDetails($id);
-
-    if(!$location){
-       F3::error(404);
-       return;
-    }
-    F3::set('location',$location);
-   
-    if(F3::get('AJAX')){
-      $ajax['coords']['lat']=$location->lat;
-      $ajax['coords']['lng']=$location->lng;
-      $pictures=App::instance()->getLocationPictures($location->id);
-      $ajax['pictures']=array_map(function($item){return array('image'=>$item->src);},$pictures);
-      echo json_encode($ajax);
-      return;
-    }
     
-    $prev=App::instance()->getPrev($location->id);
-    $next=App::instance()->getNext($location->id);
-        
-    $prev=$prev?$prev[0]['id'].'-'.$prev[0]['title']:'';
-    $next=$next?$next[0]['id'].'-'.$next[0]['title']:'';
-        
-    F3::set('prev',$prev);
-    F3::set('next',$next);
-     
-    echo Views::instance()->render('home.html');
-  }
-  
-  
-  function get(){
-    $pictures=App::instance()->getAllPictures();
-    F3::set('pictures',Views::instance()->toJson($pictures,array('image'=>'src')));
-    echo Views::instance()->render('travel.html');
-  }
-  
-  function  post(){
-    F3::set('errorMsg',null);
-    $check=array('firstname'=>'required','lastname'=>'required','email'=>'required,Audit->email','email_check'=>'=email');
-    $error=Datas::instance()->check(F3::get('POST'),$check);
-    if($error){
-      F3::set('errorMsg',$error);
-    }
-    else{
-       $contact=App::instance()->record();
-    }
-    F3::set('contact',F3::get('POST'));
+  function page(){
+    $slug=F3::get('PARAMS.slug');mlqksdjfmqlsdkjf
     
+    F3::set('menu',App::instance()->menu());
+    
+    $page=App::instance()->page($slug); 
+    F3::set('page',$page);
+    
+    F3::set('content',App::instance()->content($page->id));
+    
+    echo Views::instance()->render('layout.html');  
   }
-  
-  
-  
   
 
 }
--- app/Models/App.php
+++ app/Models/App.php
@@ -7,38 +7,19 @@ class App extends Prefab {
     F3::set('dB',new DB\SQL('mysql:host='.F3::get('db_host').';port=3306;dbname='.F3::get('db_server'),F3::get('db_login'),F3::get('db_password')));
   }
   
-  function getLocationDetails($id=null){
-    $location=new DB\SQL\Mapper(F3::get('dB'),'location');
-    if(!$id){
-      return $location->load();
-     
-    }
-    return $location->load(array('id=?',$id));
+  function menu(){
+    $menu=new DB\SQL\Mapper(F3::get('dB'),'page');
+    return $menu->find();
   }
   
-  function getLocationPictures($id){
-    $pictures=new DB\SQL\Mapper(F3::get('dB'),'pictures');
-    return $pictures->find(array('idLocation=?',$id));
+  function page($slug){
+    $page=new DB\SQL\Mapper(F3::get('dB'),'page');
+    return $page->load(array('slug=?',$slug));
   }
   
-  function getNext($id){  
-    return F3::get('dB')->exec("select id, title from location where id =(select min(id) from location where id > ".$id.")");
-  }
-  
-  function getPrev($id){
-    return F3::get('dB')->exec("select id, title from location where id =(select max(id) from location where id < ".$id.")");
-  }
-  
-  function getAllPictures(){
-    $pictures=new DB\SQL\Mapper(F3::get('dB'),'pictures');
-    return $pictures->find(array(),array('order'=>'id desc'));
-  }
-  
-  function record(){
-    $contact=new DB\SQL\Mapper(F3::get('dB'),'contact');
-    $contact->copyFrom('POST');
-    $contact->save();
-    return $contact;
+  function content($idPage){
+    $content=new DB\SQL\Mapper(F3::get('dB'),'content');
+    return $content->find(array('idPage=?',$idPage));
   }
   
   

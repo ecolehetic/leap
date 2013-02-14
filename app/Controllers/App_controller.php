a65642784c424ca1511f82ed7bc82ffd853d3bfa
fpumir <fpumir@terredeweb.fr>
2013-02-14 17:04:32 +0100

    pm g2 1.0
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -9,6 +9,17 @@ class App_controller{
     $menu=$app->getMenu();
     F3::set('menu',$menu);
     
+    $page=$app->getPage($slug);
+    if(!$page){
+      F3::error(404);
+      return;
+    }
+    F3::set('page',$page);
+    
+    $content=$app->getContent($page->id);
+    F3::set('content',$content);
+    
+    
     $views=new Views();
     echo $views->render($slug.'.html');
     

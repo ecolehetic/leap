99f5d4c427cb1a50257f9e66ba499e6096177f0c
fpumir <fpumir@terredeweb.fr>
2013-02-14 14:31:03 +0100

    pm 1.1
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -1,7 +1,28 @@
 <?php
 class App_controller{
     
-  
+  function page(){
+    
+    $slug=F3::get('PARAMS.slug');
+    
+    $app=new App();
+    $menu=$app->getMenu();
+    F3::set('menu',$menu);
+    
+    $page=$app->getPage($slug);
+    if(!$page){
+      F3::error(404);
+      return;
+    }
+    F3::set('page',$page);
+    
+    $content=$app->getContent($page->id);
+    F3::set('content',$content);
+    
+    $views=new Views();
+    echo $views->render($slug.'.html');
+    
+  }
 
 }
 

d21c1209fcf4bb02720944a56ab1fb5795ba6953
fpumir <fpumir@terredeweb.fr>
2013-02-15 08:26:50 +0100

    without async
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- public/js/leap.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$('.company-menu a').bind('click',function() {
-		$('section.container .row').load($(this).attr('href'));
-		$('.company-menu li').removeClass('active');
-		$(this).parent('li').addClass('active');
-		return false;
-	});
-});
